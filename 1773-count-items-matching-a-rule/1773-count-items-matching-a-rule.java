3b418c6075d83512153af9f66cd51a0fed448f36
AishwaryaBadri <66730581+AishwaryaBadri@users.noreply.github.com>
2022-02-04 20:11:21 +0530

    Time: 6 ms (40.21%), Space: 56.9 MB (5.70%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -6,20 +6,21 @@ class Solution {
         for(int i=0; i< items.size() ; i++)
         {
           
-              List<String> innerList=items.get(i);
+             
+            
            //   System.out.println("s-->"+s+"  s.equals(ruleValue)"+s.equals(ruleValue));
-              if(ruleKey.equals("type") && innerList.get(0).equals(ruleValue))
+              if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue))
               {
                  
                   matches++;
                   
-              }else if(ruleKey.equals("color")  && innerList.get(1).equals(ruleValue))
+              }else if(ruleKey.equals("color")  && items.get(i).get(1).equals(ruleValue))
               {
                 
                   
                       matches++;
                   
-              }else if(ruleKey.equals("name")  && innerList.get(2).equals(ruleValue))
+              }else if(ruleKey.equals("name")  && items.get(i).get(2).equals(ruleValue))
               {
                   
                   
