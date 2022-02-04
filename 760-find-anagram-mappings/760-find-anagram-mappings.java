702ad5842f4f1612757289b5076b40ef604e6ab8
AishwaryaBadri <66730581+AishwaryaBadri@users.noreply.github.com>
2022-02-04 20:39:20 +0530

    Time: 1 ms (91.53%), Space: 42.7 MB (6.08%) - LeetHub

--- 760-find-anagram-mappings/760-find-anagram-mappings.java
+++ 760-find-anagram-mappings/760-find-anagram-mappings.java
@@ -5,7 +5,7 @@ class Solution {
         
         for(int i=0;i< nums1.length; i++)
         {
-            innerloop : for(int j=0; j<nums2.length;j++)
+           for(int j=0; j<nums2.length;j++)
             {
                 if(nums1[i]==nums2[j])
                 {
