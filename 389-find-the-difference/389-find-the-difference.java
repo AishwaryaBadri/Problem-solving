6219f785a9b86c6039c196fe27a85fa7a276d36d
AishwaryaBadri <66730581+AishwaryaBadri@users.noreply.github.com>
2022-02-09 14:29:43 +0530

    Time: 4 ms (33.91%), Space: 42.3 MB (37.19%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -8,13 +8,13 @@ class Solution {
         Arrays.sort(chT);
         int i=0;
         while (i < s.length()) {
-            System.out.println("chS[i]-->"+chS[i]+" chT[i]==>"+chT[i]);
+           
             if (chS[i] != chT[i]) {
-                System.out.println(" INSIDE chS[i]-->"+chS[i]+" chT[i]==>"+chT[i]);
+               
                 return chT[i];
             }
             i += 1;
-               System.out.println(" INSIDE I"+ i);
+               
         }
         
      
