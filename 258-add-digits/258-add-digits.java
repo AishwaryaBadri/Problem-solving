5a9074596051754deff1afe896ad562408224973
AishwaryaBadri <66730581+AishwaryaBadri@users.noreply.github.com>
2022-02-09 14:55:00 +0530

    Time: 33 ms (8.45%), Space: 45.6 MB (5.50%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int addDigits(int num) {
+        
+        int len= String.valueOf(num).length();
+       
+        int sum=0;
+        String [] s=(String.valueOf(num)).split("");
+        if(len > 1)
+        {
+            while (len > 1)
+            {   
+                sum=0;
+                for(int i=0; i< len; i++)
+                { 
+                    System.out.println("s[i]" + s[i]);
+                    sum= sum +  Integer.valueOf(s[i]);
+                }
+                 s=(String.valueOf(sum)).split("");
+                 len=String.valueOf(sum).length();
+            }
+            
+        }else
+        {
+            sum=num;
+        }
+        
+        return sum;
+    }
+}
